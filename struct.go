e70916276c53b5296f09b28d32cce8919d397eaf
user <user@localhost.localdomain>
2023-08-01 00:04:18 +0300

    struct

--- struct.go
+++ struct.go
@@ -7,10 +7,23 @@ type person struct {
 	age  int
 }
 
+type address struct {
+	country string
+	city    string
+	zipCode int
+}
+
 func main() {
 
 	p := person{name: "abdullah", age: 27}
+	a := address{}
+
+	a.country = "abcdefg"
+	a.city = "abcde"
+	a.zipCode = 12344
 
 	fmt.Println("The name is : ", p.name)
 	fmt.Println("The age is : ", p.age)
+
+	fmt.Printf("from %s, %s, %d\n", a.country, a.city, a.zipCode)
 }
